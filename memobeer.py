ce371a3cbba8501aac33304fc82b231df222d828
Max Bluestone <mbluestone93@gmail.com>
2018-09-08 17:57:14 -0400

    load data and simple cluster plot

--- memobeer.py
+++ memobeer.py
@@ -1,2 +1,21 @@
 # MemoLab Beer Data Analysis
 # An analysis of the MemoLab members' beer ratings using the Hypertools package
+
+# change working directory
+import os
+os.chdir('/Users/mbluestone/Psych/')
+cwd = os.getcwd()
+
+# import pkgs
+import numpy
+import pandas
+import matplotlib
+import hypertools as hyp
+
+# import data
+beers = pandas.read_csv('Group.csv')
+print(beers.head())
+
+# plot
+hue = beers['Taster']
+plot = hyp.plot(beers, '.', hue=hue, labels=hue) # plots dots
