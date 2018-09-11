f69a9b321955be46e4ff0792c0a816c8bd3907bd
Max Bluestone <mbluestone93@gmail.com>
2018-09-11 19:32:50 -0400

    load separate taster data sets

--- memobeer.py
+++ memobeer.py
@@ -1,21 +1,32 @@
+#!/usr/local/bin/python3
 # MemoLab Beer Data Analysis
 # An analysis of the MemoLab members' beer ratings using the Hypertools package
 
 # change working directory
 import os
-os.chdir('/Users/mbluestone/Psych/')
+os.chdir('/Users/mbluestone/Psych/beerdata/')
 cwd = os.getcwd()
 
 # import pkgs
 import numpy
-import pandas
+import pandas as pd
 import matplotlib
 import hypertools as hyp
 
+## Wide Data Plotting
 # import data
-beers = pandas.read_csv('Group.csv')
-print(beers.head())
+beers = pd.read_csv('Group_Wide.csv')
+#print(beers.head())
+small_beers = beers[beers.Taster != 'Dan']
 
 # plot
-hue = beers['Taster']
-plot = hyp.plot(beers, '.', hue=hue, labels=hue) # plots dots
+hue = small_beers['Taster']
+#plot = hyp.plot(small_beers, '.', hue=hue, title='Wide Data') # plots dots
+
+## Multiple Data Sets Plotting w/ Alignment
+# import data
+ind_path = cwd + '/Individual/'
+tasters = numpy.unique(hue)
+taster_data = {}
+for f in tasters:
+    taster_data[f] = pd.read_csv(ind_path + "%s.csv" % f)
